48412fa144c5d49f0ee8dd722c8d9e12b4bc8324
hunghg255 <giahunghust@gmail.com>
2023-02-27 18:24:08 +0700

    update tokenmanager

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { extend } from 'umi-request';
 import { ENV } from 'src/utils/env';
-import TokenManagement from './tokenManagement';
+import TokenManager, { injectBearer } from 'brainless-token-manager';
 
 const REQ_TIMEOUT = 25 * 1000;
 export const isDev = ENV.NODE_ENV === 'development';
@@ -16,107 +16,35 @@ const request = extend({
   },
 });
 
-const injectBearer = (token: string, configs: any) => {
-  if (!configs) {
-    return {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-  }
+const tokenManager = new TokenManager({
+  getAccessToken: async () => {
+    // const token = getAccessToken();
 
-  if (configs?.headers?.Authorization) {
-    return {
-      ...configs,
-      headers: {
-        ...configs.headers,
-      },
-    };
-  }
-
-  if (configs?.headers) {
-    return {
-      ...configs,
-      headers: {
-        ...configs.headers,
-        Authorization: `Bearer ${token}`,
-      },
-    };
-  }
-
-  return {
-    ...configs,
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-};
-
-const TokenManager = new TokenManagement({
-  isTokenValid: () => {
-    // try {
-    //   const token = getAccessToken();
-
-    //   const decoded = parseJwt(token);
-    //   const { exp } = decoded;
-
-    //   const currentTime = Date.now() / 1000;
-
-    //   if (exp - 5 > currentTime) {
-    //     return true;
-    //   }
-
-    //   return false;
-    // } catch (error) {
-    //   return false;
-    // }
-
-    return true;
+    // return token || '';
+    return '';
   },
-  getAccessToken: () => {
-    const localInfo = window?.localStorage.getItem(ENV.LOCAL_STORAGE_KEY as string);
-    let localInfoObject;
+  getRefreshToken: async () => {
+    // const refreshToken = getRefreshToken();
 
-    if (localInfo) {
-      localInfoObject = JSON.parse(localInfo);
-    }
-
-    return localInfoObject?.token || '';
+    // return refreshToken || '';
+    return '';
   },
-  onRefreshToken() {
-    // const localInfo = window?.localStorage.getItem(ENV.LOCAL_STORAGE_KEY as string);
-    // let localInfoObject;
-    // if (localInfo) {
-    //   localInfoObject = JSON.parse(localInfo);
-    // }
-    // const refreshToken = localInfoObject?.refreshToken;
-    // if (!refreshToken) {
-    //   return done(null);
-    // }
-    // request
-    //   .post('/auth/refreshToken', {
-    //     data: {
-    //       refreshToken,
-    //     },
-    //   })
-    //   .then((result) => {
-    //     if (result.refreshToken && result.accessToken) {
-    //       done(result.accessToken);
-    //       return;
-    //     }
-    //     done(null);
-    //   })
-    //   .catch((err) => {
-    //     console.error(err);
-    //     done(null);
-    //   });
+  executeRefreshToken: async () => {
+    return {
+      token: '',
+      refresh_token: '',
+    };
+  },
+  onRefreshTokenSuccess: ({ token, refresh_token }) => {},
+  onInvalidRefreshToken: async () => {
+    // Logout
   },
 });
 
 const privateRequest = async (request: any, suffixUrl: string, configs?: any) => {
   const token: string = configs?.token
     ? configs?.token
-    : ((await TokenManager.getToken()) as string);
+    : ((await tokenManager.getToken()) as string);
 
   return request(suffixUrl, injectBearer(token, configs));
 };
